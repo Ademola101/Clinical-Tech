c38b8c31b0dcecf7e092c661d20d626f43583d73
Ademola <ademolaogunmokun@yahoo.com>
2022-03-15 18:11:48 +0100

    group model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_15_101701) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_15_142833) do
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.integer "user_id", null: false
+    t.integer "patient_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["patient_id"], name: "index_groups_on_patient_id"
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
+
   create_table "patients", force: :cascade do |t|
     t.string "name"
     t.integer "amount"
@@ -32,5 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_15_101701) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "groups", "patients"
+  add_foreign_key "groups", "users"
   add_foreign_key "patients", "users"
 end
