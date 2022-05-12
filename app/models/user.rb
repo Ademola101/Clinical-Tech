09bcb8eaf4655ad96b7e4468b2cebc2657fb4e55
Ademola <ademolaogunmokun@yahoo.com>
2022-05-12 21:32:37 +0100

    validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+         validates :username, length: { in: 3..10 }, presence: true
+         validates :email, presence: true, uniqueness: true
+         validates :password, presence: true, length: { in: 5..10 }
 has_many :groups
 has_many :patients, foreign_key: 'patient_creator_id', class_name: 'Patient'
        
